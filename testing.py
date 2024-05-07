f1a22a09fa161f82e23070f8c643fad936a019c5
xiongjeffrey <xiong.jeffrey314@gmail.com>
2024-05-07 12:37:21 -0400

    Testing updates

--- testing.py
+++ testing.py
@@ -22,14 +22,12 @@ motor = np.array(df.values)
 
 
 # BUILDING A NAIVE SIMPLICIAL COMPLEX
-    # Refer to https://simplicial.readthedocs.io/en/latest/tutorial/build-complex.html
     # Build a simplicial complex based off of vertices
 
 
 
 
 # BUILDING A TRAJECTORYMAP COMPLEX
-    # Refer to https://simplicial.readthedocs.io/en/latest/tutorial/build-complex.html
     # Build a simplicial complex based off of vertices *and* add additional edges as needed
 
 
