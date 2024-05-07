7784f28ed3d7da35a09865399ff4621f240a5398
SammyAgrawal <samart.agr@gmail.com>
2024-05-07 16:28:43 -0400

    Constructed complex in set form

--- SimplicialComplex.py
+++ SimplicialComplex.py
@@ -4,12 +4,99 @@ Class for Creating and Evaluating Feautures of Simplicial Complex
 
 
 class SimplicialComplex:
+    def __init__(self, trajectories):
+        """
+        Input: 
+            trajectories is a list of neural time series of N dimensional vectors (Nxti) matrix). Each time series is xarray data array
+            Time Series assumed to lie on k<N dimensional manifold
+            Time series length does not have to be the same, N has to be same for trajectories to exist in same space
+            I matrices of size N by Ti 
+        """
+        self.N = trajectories[0].shape[0]
+        self.trajectories = trajectories
+        self.vertices = []
+        self.edges = set()
+        self.pairwise_distances = -1
+        self.graph = -1
+        self.simplices = -1 # for k>=2
+        
+        for t in trajectories:
+            self.add_trajectory(t)
 
-    def __init__(self, trajectory):
-        # the trajectory as a N x T matrix
-        self.trajectory = trajectory
-        self.vertices = self.compute_vertices(trajectory)
-        self.edges = self.compute_edges(trajectory)
+    def reset(self):
+        self.pairwise_distances = -1
+        self.graph = -1
+        self.simplices = -1 # for k>=2
 
+    def construct_simplex(self, epsilon):
+        self.compute_pairwise_distances()
+        self.connect_edges(epsilon)
+        self.form_k_simplices(epsilon)
+
+    def add_trajectory(self, trajectory, connect_frames=True):
+        N, T = trajectory.shape
+        assert N == self.N, "trajectory dimensionality does not match" 
+        for i, t in enumerate(trajectory['T']):
+            v = trajectory.sel(T=t).data
+            v_index = len(self.vertices)
+            self.vertices.append(v)
+            if(i > 0 and connect_frames): # connect to previous state vector in trajectory
+                self.edges.add((v_index-1, v_index))
+        self.pairwise_distances = -1 # reset
+        self.graph = -1
+    
+    def compute_pairwise_distances(self):
+        vertices = np.array(self.vertices)
+        V, D = vertices.shape
+        pairwise_diff = vertices[:, np.newaxis, :] - vertices[np.newaxis, :, :]
+        pairwise_distances = np.linalg.norm(pairwise_diff, axis=2)
+        self.pairwise_distances = pairwise_distances
+        
+    def connect_edges(self, epsilon, add=True):
+        new_edges = []
+        if(type(self.pairwise_distances) == int):
+            self.compute_pairwise_distances()
+        row_indices, col_indices = np.where(self.pairwise_distances < epsilon)
+        for i, j in zip(row_indices, col_indices):
+            if(i!=j):
+                new_edges.append((i,j))
+        if(add):
+            self.edges = self.edges.union(set(new_edges))
+        return(new_edges)
+
+    def form_k_simplices(self, epsilon):
+        assert len(self.vertices) > 0, "Construct graph first"
+        assert len(self.edges) > 0, "Construct graph first"
+        graph = nx.Graph()
+        for i in range(len(self.vertices)):
+            graph.add_node(i, vector=vertices[i])
+        graph.add_edges_from(self.edges)
+        cliques = nx.find_cliques(graph)
+        extract_simplices = {}
+        for c in cliques:
+            k = len(c)
+            if(k>2): 
+                if(k not in extract_simplices.keys()):
+                    extract_simplices[k] = []
+                extract_simplices[k].append(c)
+        self.simplices = extract_simplices
+        self.graph = graph
+        return(graph, extract_simplices)
+
+    def draw(self):
+        nx.draw(self.graph, with_labels=True, node_color='skyblue', node_size=200, font_size=12)
+        plt.show()
+
+    def persist_precomputed(self, k, plot = True):
+        if(type(self.pairwise_distances) == int):
+            pairwise_distances = self.compute_pairwise_distances() 
+        rc = gudhi.RipsComplex(distance_matrix = pairwise_distances)
+        st = rc.create_simplex_tree(max_dimension=k)   
+        diagram = st.persistence()
+        if plot:
+            gudhi.plot_persistence_diagram(diagram)
+            plt.show()
+        return diagram, st
+            
     def boundary_matrix(self):
-
+        pass
--- Trajectory.py
+++ Trajectory.py
@@ -7,23 +7,61 @@ import numpy as np
 
 
 class zfSessionData:
-    def __init__(self, filename):
-        self.base_dir = "~/Documents/Geometric DA/Final Project/RamirezPaperData/analyzedBehaviorAndImaging"
-        
+    def __init__(self, fish=[], method='ep'):
+        self.base_dir = "/Users/samart/Documents/Geometric DA/Final Project/RamirezPaperData/analyzedBehaviorAndImaging"        
         self.traces = os.path.join(self.base_dir, "CaTracesAndFootprints/")
         self.behavior = os.path.join(self.base_dir, "behavior10to20Hz/")
+        # A is output of CaImAn, Gaussian Centered around cell locations
+        # A binary is a binary mask that tells where cells are; A itself pretty much useless
 
+        EP = [s for s in os.listdir(self.traces) if "traces_EPSelect" in s] # CaImAN
+        IM = [s for s in os.listdir(self.traces) if "traces_IMOpenSelect" in s] # avg intensity
+        self.files = EP if method == 'ep' else IM
+        self.method = method.lower()
+        self.fish_names = [s.split('_')[0] for s in self.files]
+        self.fish_names.sort()
+        
+        self.data = {}     
+        for f in fish:
+            print(f"Parsing {f} data")
+            self.parse_trace(f)
+
+    def get_filename(self, fish):
+        if(self.method == 'ep'):
+            return(f"{fish}_traces_EPSelect.mat")
+        elif(self.method == 'im'):
+            return(f"{fish}_traces_IMOpenSelect.mat")
+        
+    def parse_trace(self, fish):
+        filename = self.get_filename(fish)
         data = sio.loadmat(os.path.join(self.traces, filename))
-        self.cellLocs = [file[0] for file in data['localCoordinates']]
-        self.fluor = [file[0] for file in data['fluorescence']]
-        self.num_samples = self.fluor[0].shape[1] # num_cells x num_samples
+        cellLocs = [file[0] for file in data['localCoordinates']]
+        fluor = [file[0] for file in data['fluorescence']]
+        num_samples = fluor[0].shape[1] # num_cells x num_samples
+        file_data = {
+            'fluor' : fluor,
+            'cellLocations' : cellLocs,
+            'num_samples' : num_samples,
+        }
+        self.data[fish] = file_data
+        return(data)
+
+    def load_behavior(self, fish, idx):
+        fname = os.path.join(self.behavior, f"{fish}_{idx}.mat")
+        data = sio.loadmat(fname)
+        return(data)
     
-    def load_trajectory(self, idx):
+    def load_trajectory(self, fish, idx):
         # idx belongs to different OG tiff files (RHB vs non RHB?), likely z planes
-        fluor = self.fluor[idx]
+        fluor = self.data[fish]['fluor'][idx]
+        loc = self.data[fish]['cellLocations'][idx]
         # dF/F
         temporal_mean = np.mean(fluor, axis=1).reshape(-1, 1)
         dFF = (fluor - temporal_mean) / temporal_mean
-        return(dFF)
 
+        N, t = dFF.shape
+        da = xr.DataArray(dFF, dims=('N', 'T'), coords={'N' : np.arange(1, N+1), 'T' : np.arange(0, t), 
+                                                        'X' : (('N',), loc[:,0]), 'Y' : (('N',), loc[:,1])})
+        
+        return(da)
 
