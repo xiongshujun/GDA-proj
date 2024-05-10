46826e5794e18d87a480ea00202f945c3a6203ad
xiongjeffrey <xiong.jeffrey314@gmail.com>
2024-05-10 12:13:09 -0400

    Eval wrapping

--- testing_motor.py
+++ testing_motor.py
@@ -27,49 +27,53 @@ sc = SimplicialComplex([traj_sampled]) # Simplicial complex based on sampled dat
 #        EVALUATION       #
 ###########################
 
-max_epsilon = 4
+def eval_motor(twod_pca = False, threed_pca = False, tighten = True, max_epsilon = 4):
 
-# PCA comparisons
-    # Process is to PCA the data, then plot that PCA's homology diagram
+    # PCA comparisons
+        # Process is to PCA the data, then plot that PCA's homology diagram
 
-# 2D PCA
-pca_2 = PCA(n_components=2)
-pca_2.fit(traj_sampled.data.T)
-data_2d = pca_2.transform(traj_sampled.data.T)
-rc = gudhi.RipsComplex(points=data_2d, max_edge_length = max_epsilon)
-st = rc.create_simplex_tree(max_dimension=2)
+    # 2D PCA
+    if twod_pca:
+        pca_2 = PCA(n_components=2)
+        pca_2.fit(traj_sampled.data.T)
+        data_2d = pca_2.transform(traj_sampled.data.T)
+        rc = gudhi.RipsComplex(points=data_2d, max_edge_length = max_epsilon)
+        st = rc.create_simplex_tree(max_dimension=2)
 
-diagram = st.persistence()
-gudhi.plot_persistence_barcode(diagram)
-plt.title("Persistence Barcode of 2D PCA")
-plt.show()
+        diagram = st.persistence()
+        gudhi.plot_persistence_barcode(diagram)
+        plt.title("Persistence Barcode of 2D PCA")
+        plt.show()
 
-gudhi.plot_persistence_diagram(diagram)
-plt.title("Persistence Diagram of 2D PCA")
-plt.show()
+        gudhi.plot_persistence_diagram(diagram)
+        plt.title("Persistence Diagram of 2D PCA")
+        plt.show()
 
 
-# 3D PCA
-pca_3 = PCA(n_components=3)
-pca_3.fit(traj_sampled.data.T)
-data_3d = pca_3.transform(traj_sampled.data.T)
-rc = gudhi.RipsComplex(points=data_3d, max_edge_length = max_epsilon)
-st = rc.create_simplex_tree(max_dimension=3)
+    # 3D PCA
+    if threed_pca:
+        pca_3 = PCA(n_components=3)
+        pca_3.fit(traj_sampled.data.T)
+        data_3d = pca_3.transform(traj_sampled.data.T)
+        rc = gudhi.RipsComplex(points=data_3d, max_edge_length = max_epsilon)
+        st = rc.create_simplex_tree(max_dimension=3)
 
-diagram = st.persistence()
-gudhi.plot_persistence_barcode(diagram)
-plt.title("Persistence Barcode of 3D PCA")
-plt.show()
+        diagram = st.persistence()
+        gudhi.plot_persistence_barcode(diagram)
+        plt.title("Persistence Barcode of 3D PCA")
+        plt.show()
 
-gudhi.plot_persistence_diagram(diagram)
-plt.title("Persistence Diagram of 3D PCA")
-plt.show()
+        gudhi.plot_persistence_diagram(diagram)
+        plt.title("Persistence Diagram of 3D PCA")
+        plt.show()
 
-# Epsilon Tightening
-    # Persistent Homology diagrams drawn via plot = True
+    # Epsilon Tightening
+        # Persistent Homology diagrams drawn via plot = True
+    if tighten:
+        betti_diff, acc_vr, acc_tm = epsilon_tighten(sc, [traj_sampled_test], max_epsilon = max_epsilon, plot = True)
 
-betti_diff, acc_vr, acc_tm = epsilon_tighten(sc, [traj_sampled_test], max_epsilon = max_epsilon, plot = True)
+        print("The difference in Betti values at different levels is: " + betti_diff)
+        print("The accuracy of Vietoris-Rips complexes is: " + acc_vr)
+        print("The accuracy of TrajectoryMap is: " + acc_tm)
 
-print("The difference in Betti values at different levels is: " + betti_diff)
-print("The accuracy of Vietoris-Rips complexes is: " + acc_vr)
-print("The accuracy of TrajectoryMap is: " + acc_tm)
+eval_motor()
